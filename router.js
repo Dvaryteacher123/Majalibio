6e3456d9869a2da8df8d72b106a28dff93035a71
Dvaryteacher123 <dullamanyama0@gmail.com>
2026-09-10 16:52:42 +0300

    Update router.js

--- router.js
+++ router.js
@@ -2,7 +2,8 @@ const express = require('express');
 const router = express.Router();
 const paymentController = require('./controllers/paymentController');
 
-router.post('/pay', paymentController.fanyaMalipoFimi);
+// Hapa ndipo njia za malipo na webhook zinapounganishwa na mpishi sahihi
+router.post('/pay', paymentController.fanyaMalipoHarakaPay);
+router.post('/webhook/harakapay', paymentController.pokeaWebhookHarakaPay);
 
 module.exports = router;
-
